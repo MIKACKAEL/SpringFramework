8ddb735373cba37f297713b44830d67227050330
Mika <hajarison02@gmail.com>
2024-06-23 13:22:05 +0300

    Sprint 4

--- Framework/src/Controller/HelloController.java
+++ Framework/src/Controller/HelloController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Annotations.Controller;
 import Annotations.Get;
+import mg.prom16.ModelView;
 
 @Controller
 public class HelloController {
@@ -9,5 +10,13 @@ public class HelloController {
     @Get(value = "/helloWorld")
     public String helloWorld() {
         return "Hello Workd";
+    }
+
+    @Get(value = "/helloWorldPage")
+    public ModelView helloWorldView() { 
+        ModelView modelView = new ModelView();
+        modelView.setUrl("/views/helloWorldPage.jsp");
+        modelView.addObject("message", "Hello World");
+        return modelView;
     }
 }
